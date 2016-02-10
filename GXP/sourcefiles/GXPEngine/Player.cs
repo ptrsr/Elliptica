02956eec5225aa8c7567efbcc601384044814440
Ruud Peters <ruud_peters96@hotmail.com>
2016-02-10 06:12:11 -0800

    Animation

--- GXP/sourcefiles/GXPEngine/Player.cs
+++ GXP/sourcefiles/GXPEngine/Player.cs
@@ -16,7 +16,10 @@ namespace GXPEngine
         private Vec2 _acceleration;
         private Vec2 _gravity = new Vec2(0, 1);
         private bool onGround = false;
-        private float frame = 17;
+        private float frame = 0.0f;
+        private float firstframe = 0.0f;
+        private float lastframe = 0.0f;
+        private int _directionX;
         private Arm arm;
 
         public Player(Vec2 pPosition = null) : base("player.png", 16 , 2)
@@ -36,6 +39,7 @@ namespace GXPEngine
 
         void Update()
         {
+            UpdateAnimation();
             Movements();
             Step();
             acceleration = Vec2.zero;
@@ -50,21 +54,32 @@ namespace GXPEngine
                 if (onGround)
                 {
                     acceleration.Add(new Vec2(0, -20));
+                    Console.WriteLine(true);
+
                 }
             }
             if (Input.GetKey(Key.D))
             {
                 acceleration.Add(new Vec2(1, 0));
+                firstframe = 0;
+                lastframe = 15;
+                _directionX = 1;
             }
-            if (Input.GetKey(Key.A))
+            else if (Input.GetKey(Key.A))
             {
-                acceleration.Add(new Vec2(-1,0));
+                acceleration.Add(new Vec2(-1, 0));
+                firstframe = 0;
+                lastframe = 15;
+                _directionX = -1;
             }
-            if (Input.GetMouseButtonDown(0))
-            {
-                arm.Shooting();
+            else {
+                firstframe = 16;
+                lastframe = 22;
+                onGround = false;
             }
-            onGround = false;
+            scaleX = _directionX;
+        
+        onGround = false;
         }
 
         public Vec2 position
@@ -117,6 +132,16 @@ namespace GXPEngine
        public void SetOnGound()
         {
             onGround = true;
+        }
+
+        void UpdateAnimation()
+        {
+            frame = frame + 0.2f;
+            if (frame >= lastframe + 1)
+                frame = firstframe;
+            if (frame < firstframe)
+                frame = lastframe;
+            SetFrame((int)frame);
         }
     }
 }
